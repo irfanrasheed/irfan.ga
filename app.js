700df4cc91fd1253dc25d2b4858b96db51ae4de0
irfanrasheed <irfanrasheed006@gmail.com>
2019-11-26 15:26:38 +0200

    change server port

--- app.js
+++ app.js
@@ -60,9 +60,8 @@ app.get ('/*', function (req, res) {
 });
 
 //stating the app  @TODO move this to DB connection callback
-  //  app.listen(3000, function () {
-  //    console.log('Example app listening on port 3000!');
-  
-// );
+    app.listen(3000, function () {
+     console.log('Example app listening on port 3000!');
+   );
 
 
